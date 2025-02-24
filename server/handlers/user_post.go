38b100350095160d15fb7f69da6efff701ff91de
josuetorr <josue.dtorres@proton.me>
2025-02-24 02:30:13 -0500

    added post user handler

--- server/handlers/user_post.go
+++ server/handlers/user_post.go
@@ -1,1 +1,11 @@
 package handlers
+
+import "net/http"
+
+type PostUserHandler struct{}
+
+func NewPostUserHandler() *PostUserHandler {
+	return &PostUserHandler{}
+}
+
+func (h *PostUserHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {}
--- server/routes/users.go
+++ server/routes/users.go
@@ -2,10 +2,13 @@ package routes
 
 import (
 	"github.com/go-chi/chi"
+	"github.com/josuetorr/frequent-flyer/server/handlers"
 )
 
 func NewUserRoutes() chi.Router {
 	r := chi.NewRouter()
+
+	r.Post("/", handlers.NewPostUserHandler().ServeHTTP)
 
 	return r
 }
