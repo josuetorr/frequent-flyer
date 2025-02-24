526b671f661ccae75e4e08d998da9854deae9fcd
josuetorr <josue.dtorres@proton.me>
2025-02-24 01:27:49 -0500

    calling godotenv manually

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,11 +3,14 @@ package main
 import (
 	"context"
 
-	_ "github.com/joho/godotenv/autoload"
+	"github.com/joho/godotenv"
+	_ "github.com/joho/godotenv"
 	"github.com/josuetorr/frequent-flyer/server/data"
 )
 
 func main() {
+	godotenv.Load(".dev.env")
+
 	ctx := context.Background()
 	dbPool := data.Init(ctx)
 	defer dbPool.Close()
