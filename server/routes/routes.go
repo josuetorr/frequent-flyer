fc7bf7214a6ea6d3a0632742e8cdb50e01c9614e
josuetorr <josue.dtorres@proton.me>
2025-02-24 03:10:52 -0500

    fixed chi import

--- server/routes/routes.go
+++ server/routes/routes.go
@@ -1,17 +1,19 @@
 package routes
 
 import (
-	"github.com/go-chi/chi/v5"
-	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
 )
 
 func RegisterRoutes() chi.Router {
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
 
-	r.Mount("/users", NewUserRoutes())
-	r.Mount("/products", NewProductsRoutes())
-	r.Mount("/stores", NewStoreRoutes())
+	r.Route("/api/v1", func(r chi.Router) {
+		r.Mount("/users", NewUserRoutes())
+		r.Mount("/products", NewProductsRoutes())
+		r.Mount("/stores", NewStoreRoutes())
+	})
 
 	return r
 }
