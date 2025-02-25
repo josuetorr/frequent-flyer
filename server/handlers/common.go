9a3214a6b060cd9c9d31392e3bd8faf9b8b5d8ca
josuetorr <josue.dtorres@proton.me>
2025-02-25 00:01:26 -0500

    able to update a user

--- server/handlers/common.go
+++ server/handlers/common.go
@@ -14,4 +14,5 @@ type (
 type UserService interface {
 	Insert(context.Context, *models.User) error
 	Get(context.Context, ID) (*User, error)
+	Update(context.Context, ID, *User) error
 }
--- server/handlers/user_put.go
+++ server/handlers/user_put.go
@@ -1,11 +1,44 @@
 package handlers
 
-import "net/http"
+import (
+	"encoding/json"
+	"log/slog"
+	"net/http"
 
-type PutUserHandler struct{}
+	"github.com/go-chi/chi"
+)
 
-func NewPutUserHanlder() PutUserHandler {
-	return PutUserHandler{}
+type PutUserHandler struct {
+	log         *slog.Logger
+	userService UserService
 }
 
-func (h PutUserHandler) ServeHttp(w http.ResponseWriter, r *http.Request) {}
+func NewPutUserHanlder(log *slog.Logger, service UserService) *PutUserHandler {
+	return &PutUserHandler{log: log, userService: service}
+}
+
+func (h *PutUserHandler) ServeHttp(w http.ResponseWriter, r *http.Request) {
+	ct := r.Header.Get("Content-Type")
+	if ct != "application/json" {
+		http.Error(w, "Unsupported Media Type", http.StatusUnsupportedMediaType)
+		return
+	}
+
+	id := chi.URLParam(r, "id")
+	defer r.Body.Close()
+
+	var updatedUser User
+	if err := json.NewDecoder(r.Body).Decode(&updatedUser); err != nil {
+		h.log.Error(err.Error())
+		http.Error(w, "Invalid json", http.StatusBadRequest)
+		return
+	}
+
+	if err := h.userService.Update(r.Context(), id, &updatedUser); err != nil {
+		h.log.Error(err.Error())
+		http.Error(w, "Internal server error", http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+}
