6acae75cc31f07658db8c221623bbb9a64352166
josuetorr <josue.dtorres@proton.me>
2025-02-26 01:31:08 -0500

    auth works!

---
+++ server/middleware/auth.go
@@ -0,0 +1,46 @@
+package middleware
+
+import (
+	"errors"
+	"fmt"
+	"net/http"
+	"strings"
+
+	"github.com/golang-jwt/jwt/v5"
+)
+
+func AuthMiddleware(next http.Handler) http.Handler {
+	fn := func(w http.ResponseWriter, r *http.Request) {
+		authHeader := r.Header.Get("Authorization")
+		if authHeader == "" {
+			http.Error(w, "Unauthorized", http.StatusUnauthorized)
+			return
+		}
+
+		tokenStr := strings.TrimPrefix(authHeader, "Bearer ")
+		token, err := jwt.Parse(tokenStr, func(token *jwt.Token) (interface{}, error) {
+			return []byte("some-secret"), nil
+		})
+
+		// TODO: clean this up
+		switch {
+		case token.Valid:
+			next.ServeHTTP(w, r)
+		case errors.Is(err, jwt.ErrTokenMalformed):
+			http.Error(w, fmt.Sprint("That's not even a token"), http.StatusUnauthorized)
+			return
+		case errors.Is(err, jwt.ErrTokenSignatureInvalid):
+			// Invalid signature
+			http.Error(w, fmt.Sprint("Invalid signature"), http.StatusUnauthorized)
+			return
+		case errors.Is(err, jwt.ErrTokenExpired) || errors.Is(err, jwt.ErrTokenNotValidYet):
+			// Token is either expired or not active yet
+			http.Error(w, fmt.Sprint("Timing is everything"), http.StatusUnauthorized)
+			return
+		default:
+			http.Error(w, fmt.Sprint("Couldn't handle this token:"), http.StatusUnauthorized)
+			return
+		}
+	}
+	return http.HandlerFunc(fn)
+}
--- server/routes/routes.go
+++ server/routes/routes.go
@@ -4,19 +4,21 @@ import (
 	"log/slog"
 
 	"github.com/go-chi/chi"
-	"github.com/go-chi/chi/middleware"
+	cm "github.com/go-chi/chi/middleware"
 	"github.com/josuetorr/frequent-flyer/server/data"
+	m "github.com/josuetorr/frequent-flyer/server/middleware"
 	"github.com/josuetorr/frequent-flyer/server/utils"
 )
 
 func RegisterRoutes(log *slog.Logger, db *data.DBPool) chi.Router {
 	r := chi.NewRouter()
-	r.Use(middleware.Logger)
+	r.Use(cm.Logger)
 
 	userRepo := data.NewUserRepositor(db)
 
 	r.Route("/api/v1", func(r chi.Router) {
-		r.Use(middleware.AllowContentType(utils.ContentTypeJSON))
+		r.Use(cm.AllowContentType(utils.ContentTypeJSON))
+		r.Use(m.AuthMiddleware)
 
 		r.Mount("/auth", NewAuthRoutes(userRepo))
 		r.Mount("/users", NewUserRoutes(userRepo))
