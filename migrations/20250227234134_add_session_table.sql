1836f0a0d771a32df5aea2149e7432fa638da738
josuetorr <josue.dtorres@proton.me>
2025-02-27 18:48:17 -0500

    fixed typo in session migration
    create_at -> created_at

--- migrations/20250227234134_add_session_table.sql
+++ migrations/20250227234134_add_session_table.sql
@@ -3,7 +3,7 @@
 CREATE TABLE IF NOT EXISTS sessions(
   id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
   user_id UUID NOT NULL,
-  create_at TIMESTAMP NOT NULL,
+  created_at TIMESTAMP NOT NULL,
   expires_in INTEGER NOT NULL,
   FOREIGN KEY(user_id) REFERENCES users(id) ON DELETE CASCADE
 );
