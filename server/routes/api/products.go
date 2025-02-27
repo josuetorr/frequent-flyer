1fecbb65ded513ebc712da7eecdb0772c4d80b45
josuetorr <josue.dtorres@proton.me>
2025-02-26 21:20:37 -0500

    moves routes to api dir

--- server/routes/api/products.go
+++ server/routes/api/products.go
@@ -1,10 +1,10 @@
-package routes
+package api
 
 import (
 	"github.com/go-chi/chi"
 )
 
-func NewStoreRoutes() chi.Router {
+func NewProductsRoutes() chi.Router {
 	r := chi.NewRouter()
 
 	return r
--- server/routes/api/routes.go
+++ server/routes/api/routes.go
@@ -1,4 +1,4 @@
-package routes
+package api
 
 import (
 	"github.com/go-chi/chi"
