77d1df55430f71c651821d3a6843da00af5ae93e
josuetorr <josue.dtorres@proton.me>
2025-02-27 17:40:32 -0500

    added base layout

--- web/templates/Login_templ.go
+++ web/templates/Login_templ.go
@@ -8,6 +8,8 @@ package templates
 import "github.com/a-h/templ"
 import templruntime "github.com/a-h/templ/runtime"
 
+import "github.com/josuetorr/frequent-flyer/web/templates/layouts"
+
 func Login() templ.Component {
 	return templruntime.GeneratedTemplate(func(templ_7745c5c3_Input templruntime.GeneratedComponentInput) (templ_7745c5c3_Err error) {
 		templ_7745c5c3_W, ctx := templ_7745c5c3_Input.Writer, templ_7745c5c3_Input.Context
@@ -29,7 +31,25 @@ func Login() templ.Component {
 			templ_7745c5c3_Var1 = templ.NopComponent
 		}
 		ctx = templ.ClearChildren(ctx)
-		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 1, "<!doctype html><html data-theme=\"retro\"><head><link rel=\"stylesheet\" href=\"public/styles/style.css\"><script src=\"public/js/htmx.js\" defer></script></head><body><div class=\"h-screen flex justify-center items-center\"><form class=\"flex justify-center items-center card bg-base-200 w-96 shadow-sm gap-2 p-2\"><h2>Frequent Flyers</h2><label class=\"input\"><input type=\"text\" name=\"email\" placeholder=\"email\"></label> <label class=\"input\"><input type=\"password\" name=\"password\" placeholder=\"password\"></label><div class=\"card-actions\"><button type=\"submit\" class=\"btn bg-base-100\">login</button></div><p>don't have an account? <a class=\"link-accent\" href=\"/signup\">Signup</a></p></form></div></body></html>")
+		templ_7745c5c3_Var2 := templruntime.GeneratedTemplate(func(templ_7745c5c3_Input templruntime.GeneratedComponentInput) (templ_7745c5c3_Err error) {
+			templ_7745c5c3_W, ctx := templ_7745c5c3_Input.Writer, templ_7745c5c3_Input.Context
+			templ_7745c5c3_Buffer, templ_7745c5c3_IsBuffer := templruntime.GetBuffer(templ_7745c5c3_W)
+			if !templ_7745c5c3_IsBuffer {
+				defer func() {
+					templ_7745c5c3_BufErr := templruntime.ReleaseBuffer(templ_7745c5c3_Buffer)
+					if templ_7745c5c3_Err == nil {
+						templ_7745c5c3_Err = templ_7745c5c3_BufErr
+					}
+				}()
+			}
+			ctx = templ.InitializeContext(ctx)
+			templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 1, "<div class=\"h-screen flex justify-center items-center\"><form class=\"flex justify-center items-center card bg-base-200 w-96 shadow-sm gap-2 p-2\"><h2>Frequent Flyers</h2><label class=\"input\"><input type=\"text\" name=\"email\" placeholder=\"email\"></label> <label class=\"input\"><input type=\"password\" name=\"password\" placeholder=\"password\"></label><div class=\"card-actions\"><button type=\"submit\" class=\"btn bg-base-100\">login</button></div><p>don't have an account? <a class=\"link-accent\" href=\"/signup\">Signup</a></p></form></div>")
+			if templ_7745c5c3_Err != nil {
+				return templ_7745c5c3_Err
+			}
+			return nil
+		})
+		templ_7745c5c3_Err = layouts.BaseLayout().Render(templ.WithChildren(ctx, templ_7745c5c3_Var2), templ_7745c5c3_Buffer)
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
