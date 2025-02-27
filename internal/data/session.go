6b05e881efc39e2099104a8ebd499d9a2a28de76
josuetorr <josue.dtorres@proton.me>
2025-02-27 18:55:54 -0500

    added getbyid

--- internal/data/session.go
+++ internal/data/session.go
@@ -2,7 +2,10 @@ package data
 
 import "context"
 
-const createSessionQuery = "INSERT INTO sessions (user_id, created_at, expires_in) VALUES ($1, $2, $3)"
+const (
+	createSessionQuery     = "INSERT INTO sessions (user_id, created_at, expires_in) VALUES ($1, $2, $3)"
+	selectSessionByIdQuery = "SELECT * FROM sessions WHERE id = $1"
+)
 
 type SessionRepository struct {
 	db *DBPool
@@ -15,4 +18,19 @@ func NewSessionRepository(db *DBPool) *SessionRepository {
 func (r *SessionRepository) Insert(ctx context.Context, s *Session) error {
 	_, err := r.db.Exec(ctx, createSessionQuery, s.UserID, s.CreatedAt, s.ExpiresIn)
 	return err
+}
+
+func (r *SessionRepository) GetById(ctx context.Context, id ID) (*Session, error) {
+	row, err := r.db.Query(ctx, selectSessionByIdQuery, id)
+	if err != nil {
+		return nil, err
+	}
+
+	var s Session
+	err = row.Scan(&s.ID, &s.UserID, &s.CreatedAt, &s.ExpiresIn)
+	if err != nil {
+		return nil, err
+	}
+
+	return &s, nil
 }
--- server/models/session.go
+++ server/models/session.go
@@ -6,5 +6,5 @@ type Session struct {
 	ID        ID
 	UserID    ID
 	CreatedAt *time.Time
-	ExpiresId uint
+	ExpiresIn uint
 }
