dffa45d96291f14f70a84894845bd3a623ab4608
josuetorr <josue.dtorres@proton.me>
2025-02-27 20:09:54 -0500

    removed api

--- server/routes/routes.go
+++ server/routes/routes.go
@@ -4,8 +4,6 @@ import (
 	"github.com/go-chi/chi"
 	cm "github.com/go-chi/chi/middleware"
 	"github.com/josuetorr/frequent-flyer/internal/data"
-	"github.com/josuetorr/frequent-flyer/internal/services"
-	"github.com/josuetorr/frequent-flyer/server/routes/api"
 	"github.com/josuetorr/frequent-flyer/server/routes/web"
 )
 
@@ -13,11 +11,6 @@ func RegisterRoutes(db *data.DBPool) chi.Router {
 	r := chi.NewRouter()
 	r.Use(cm.Logger)
 
-	userRepo := data.NewUserRepository(db)
-	userService := services.NewUserService(userRepo)
-	authService := services.NewAuthService(userRepo)
-
-	r.Mount("/api/v1", api.RegisterApiRoutes(authService, userService))
 	r.Mount("/", pages.RegisterWebRoutes())
 
 	return r
