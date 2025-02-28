16e0acc6f964f11afc03ea95f944b2aa7d14f0e1
josuetorr <josue.dtorres@proton.me>
2025-02-27 23:31:48 -0500

    renamed var

--- server/internal/middleware/auth.go
+++ server/internal/middleware/auth.go
@@ -5,29 +5,30 @@ import (
 	"log/slog"
 	"net/http"
 
-	"github.com/josuetorr/frequent-flyer/internal/models"
 	"github.com/josuetorr/frequent-flyer/server/handlers"
 	"github.com/josuetorr/frequent-flyer/server/internal/utils"
 )
 
+type LoggedUser = string
+
 func AuthMiddlerware(sessionService handlers.SessionService) func(http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
 		fn := func(w http.ResponseWriter, r *http.Request) {
-			sessionToken, err := r.Cookie(utils.SessionCookieName)
+			sessionCookie, err := r.Cookie(utils.SessionCookieName)
 			if err != nil {
 				slog.Error(err.Error())
 				http.Error(w, "Unauthorized", http.StatusUnauthorized)
 				return
 			}
 
-			s, err := sessionService.GetByToken(r.Context(), sessionToken.Value)
+			s, err := sessionService.GetByToken(r.Context(), sessionCookie.Value)
 			if err != nil {
 				slog.Error(err.Error())
 				http.Error(w, "Unauthorized", http.StatusUnauthorized)
 				return
 			}
 
-			ctx := context.WithValue(r.Context(), models.ID(s.UserID), s.UserID)
+			ctx := context.WithValue(r.Context(), LoggedUser(s.UserID), s.UserID)
 			next.ServeHTTP(w, r.WithContext(ctx))
 		}
 		return http.HandlerFunc(fn)
