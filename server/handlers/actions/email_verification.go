abea52b886d0338303ee4b0c97574a540202233a
josuetorr <josue.dtorres@proton.me>
2025-03-03 22:15:20 -0500

    added logic to verify user

--- server/handlers/actions/email_verification.go
+++ server/handlers/actions/email_verification.go
@@ -1,11 +1,41 @@
 package actions
 
-import "net/http"
+import (
+	"log/slog"
+	"net/http"
 
-type EmailVerificationHandler struct{}
+	"github.com/go-chi/chi"
+	au "github.com/josuetorr/frequent-flyer/internal/utils"
+	"github.com/josuetorr/frequent-flyer/server/handlers"
+	su "github.com/josuetorr/frequent-flyer/server/internal/utils"
+)
 
-func NewEmailVerificationHandler() *EmailVerificationHandler {
-	return &EmailVerificationHandler{}
+type EmailVerificationHandler struct {
+	authService handlers.AuthService
 }
 
-func (h *EmailVerificationHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {}
+func NewEmailVerificationHandler(authService handlers.AuthService) *EmailVerificationHandler {
+	return &EmailVerificationHandler{
+		authService: authService,
+	}
+}
+
+func (h *EmailVerificationHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	token := chi.URLParam(r, "token")
+
+	userID, err := su.VerifyToken(token, au.GetEmailVerificationSecret())
+	if err != nil {
+		slog.Error(err.Error())
+		http.Error(w, "Resource not found", http.StatusBadRequest)
+		return
+	}
+
+	if err := h.authService.VerifyUser(r.Context(), userID); err != nil {
+		slog.Error(err.Error())
+		http.Error(w, "Internal server error", http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("HX-REDIRECT", "/home")
+	w.WriteHeader(http.StatusNoContent)
+}
