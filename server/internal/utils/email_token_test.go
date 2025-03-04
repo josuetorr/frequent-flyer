03de11db8e1abad37ba4d1fb858f377d4f55ab5d
josuetorr <josue.dtorres@proton.me>
2025-03-03 19:40:13 -0500

    added test for email tokens

---
+++ server/internal/utils/email_token_test.go
@@ -0,0 +1,33 @@
+package utils
+
+import "testing"
+
+func TestValidToken(t *testing.T) {
+	secret := "bob"
+	expectedUserId := "123"
+	token := GenerateEmailToken(expectedUserId, secret)
+	result, err := VerifyToken(token, secret)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if result != expectedUserId {
+		t.Errorf("expected userID: %s. Received: %s", expectedUserId, result)
+	}
+}
+
+func TestInvalidSignedToken(t *testing.T) {
+	secret := "bob"
+	invalidSecret := "obo"
+	expectedUserId := "123"
+
+	token := GenerateEmailToken(expectedUserId, secret)
+	result, err := VerifyToken(token, invalidSecret)
+	if err == nil {
+		t.Error("Verify should have returning an error since signatures should not match")
+	}
+
+	if result == expectedUserId {
+		t.Errorf("expected userID: %s. Received: %s. Results should be different", expectedUserId, result)
+	}
+}
