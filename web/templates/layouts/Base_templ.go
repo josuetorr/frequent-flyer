1ff39988e10c956b79878775804af89cb39b7259
josuetorr <josue.dtorres@proton.me>
2025-03-04 19:52:59 -0500

    fixed base layout's imports

--- web/templates/layouts/Base_templ.go
+++ web/templates/layouts/Base_templ.go
@@ -29,7 +29,7 @@ func BaseLayout() templ.Component {
 			templ_7745c5c3_Var1 = templ.NopComponent
 		}
 		ctx = templ.ClearChildren(ctx)
-		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 1, "<!doctype html><html data-theme=\"retro\"><head><link rel=\"stylesheet\" href=\"public/styles/style.css\"><script src=\"public/js/htmx.js\" defer></script></head><body>")
+		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 1, "<!doctype html><html data-theme=\"retro\"><head><link rel=\"stylesheet\" href=\"/public/styles/style.css\"><script src=\"/public/js/htmx.js\" defer></script></head><body>")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
