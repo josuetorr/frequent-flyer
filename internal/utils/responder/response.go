fbf8c54b71b945daaeec06308aef241195317071
josuetorr <josue.dtorres@proton.me>
2025-03-05 23:37:45 -0500

    added response constructors

--- internal/utils/responder/response.go
+++ internal/utils/responder/response.go
@@ -12,7 +12,11 @@ type AppResponse struct {
 	Component  templ.Component
 }
 
-func NewAppResponse(statusCode int, header http.Header, component templ.Component) *AppResponse {
+func (res *AppResponse) Respond(w http.ResponseWriter, r *http.Request) {
+	respond(w, r, res.Header, res.StatusCode, res.Component)
+}
+
+func newAppResponse(statusCode int, header http.Header, component templ.Component) *AppResponse {
 	return &AppResponse{
 		StatusCode: statusCode,
 		Header:     header,
@@ -20,6 +24,22 @@ func NewAppResponse(statusCode int, header http.Header, component templ.Componen
 	}
 }
 
-func (res *AppResponse) Respond(w http.ResponseWriter, r *http.Request) {
-	respond(w, r, res.Header, res.StatusCode, res.Component)
+func NewOk(h http.Header, c templ.Component) *AppResponse {
+	return newAppResponse(http.StatusOK, h, c)
+}
+
+func NewCreated(h http.Header, c templ.Component) *AppResponse {
+	return newAppResponse(http.StatusCreated, h, c)
+}
+
+func NewAccepted(h http.Header, c templ.Component) *AppResponse {
+	return newAppResponse(http.StatusAccepted, h, c)
+}
+
+func NewNoContent(h http.Header, c templ.Component) *AppResponse {
+	return newAppResponse(http.StatusNoContent, h, c)
+}
+
+func NewSeeOther(h http.Header, c templ.Component) *AppResponse {
+	return newAppResponse(http.StatusSeeOther, h, c)
 }
