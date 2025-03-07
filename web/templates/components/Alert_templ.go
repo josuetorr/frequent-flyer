99eac5837501a06ef73148da9d2fb63f95d6dcc7
josuetorr <josue.dtorres@proton.me>
2025-03-06 22:05:22 -0500

    removed full width from alert

--- web/templates/components/Alert_templ.go
+++ web/templates/components/Alert_templ.go
@@ -38,7 +38,7 @@ func alert(message string, severity Severity) templ.Component {
 			templ_7745c5c3_Var1 = templ.NopComponent
 		}
 		ctx = templ.ClearChildren(ctx)
-		var templ_7745c5c3_Var2 = []any{fmt.Sprintf("alert alert-%s alert-soft w-full", severity)}
+		var templ_7745c5c3_Var2 = []any{fmt.Sprintf("alert alert-%s alert-soft", severity)}
 		templ_7745c5c3_Err = templ.RenderCSSItems(ctx, templ_7745c5c3_Buffer, templ_7745c5c3_Var2...)
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
@@ -63,7 +63,7 @@ func alert(message string, severity Severity) templ.Component {
 		var templ_7745c5c3_Var4 string
 		templ_7745c5c3_Var4, templ_7745c5c3_Err = templ.JoinStringErrs(message)
 		if templ_7745c5c3_Err != nil {
-			return templ.Error{Err: templ_7745c5c3_Err, FileName: `web/templates/components/Alert.templ`, Line: 13, Col: 94}
+			return templ.Error{Err: templ_7745c5c3_Err, FileName: `web/templates/components/Alert.templ`, Line: 13, Col: 87}
 		}
 		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString(templ_7745c5c3_Var4))
 		if templ_7745c5c3_Err != nil {
