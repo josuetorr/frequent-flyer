aa792540a3f4bd90f4057efa1a19951c425fee45
josuetorr <josue.dtorres@proton.me>
2025-03-08 18:36:25 -0500

    using mockgen to test http handlers

--- go.mod
+++ go.mod
@@ -5,15 +5,16 @@ go 1.24.0
 require (
 	github.com/a-h/templ v0.3.833
 	github.com/go-chi/chi v1.5.5
+	github.com/gorilla/securecookie v1.1.2
 	github.com/jackc/pgx/v5 v5.7.2
 	github.com/joho/godotenv v1.5.1
+	go.uber.org/mock v0.5.0
 	golang.org/x/crypto v0.31.0
 	gopkg.in/gomail.v2 v2.0.0-20160411212932-81ebce5c23df
 )
 
 require (
 	github.com/davecgh/go-spew v1.1.2-0.20180830191138-d8f796af33cc // indirect
-	github.com/gorilla/securecookie v1.1.2 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
 	github.com/jackc/puddle/v2 v2.2.2 // indirect
---
+++ server/handlers/actions/email_verification_test.go
@@ -0,0 +1,42 @@
+package actions
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/go-chi/chi"
+	"github.com/josuetorr/frequent-flyer/internal/models"
+	"github.com/josuetorr/frequent-flyer/internal/utils"
+	emailtoken "github.com/josuetorr/frequent-flyer/internal/utils/email_token"
+	"github.com/josuetorr/frequent-flyer/server/handlers"
+	"go.uber.org/mock/gomock"
+)
+
+func TestHandleEmailVerification_Successful(t *testing.T) {
+	// setup
+	testUser := &models.User{ID: "123"}
+	ctrl := gomock.NewController(t)
+	mockUserService := handlers.NewMockUserService(ctrl)
+	mockUserService.EXPECT().
+		GetById(gomock.Any(), testUser.ID).
+		Return(testUser, nil)
+	mockUserService.EXPECT().
+		VerifyUser(gomock.Any(), testUser.ID).
+		Return(nil)
+
+	r := chi.NewRouter()
+	r.Get("/verify-email/{token}", HandleEmailVerification(mockUserService).ServeHTTP)
+	token := emailtoken.GenerateEmailToken("123", utils.GetEmailSecret())
+	req := httptest.NewRequest(http.MethodGet, "/verify-email/"+token, nil)
+	rw := httptest.NewRecorder()
+
+	// act
+	r.ServeHTTP(rw, req)
+	res := rw.Result()
+
+	// assert
+	if res.StatusCode != http.StatusSeeOther {
+		t.Errorf("Expected a redirection to /login. Received: %s", res.Header.Get("Location"))
+	}
+}
